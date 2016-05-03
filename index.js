6f2330aaa009c20b8cdfcfc985ca4fc2508fcbac
Luca Becker <luca.becker@me.com>
2016-05-03 09:55:58 +0200

    slight improvements

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var Service;
 var Characteristic;
 var HomebridgeAPI;
-var Gpio = require('onoff').Gpio
-var contactSensor;
+var Gpio = require('onoff').Gpio;
+
+var RETRY_COUNT = 10;
 
 
 module.exports = function(homebridge) {
@@ -12,13 +13,14 @@ module.exports = function(homebridge) {
 
     // console.log(Service.ContactSensor);
     homebridge.registerAccessory("homebridge-contact-gpio-sensor", "ContactGPIOSensor", ContactGPIOSensor);
-}
+};
 
 function ContactGPIOSensor(log, config) {
 	this.log = log;
 	this.name = config.name;
 	this.pinId = config.pinId;
-	contactSensor = new Gpio(this.pinId, 'in', 'both');
+	this.retryCount = config.retryCount || RETRY_COUNT;
+	this.contactSensor = new Gpio(this.pinId, 'in', 'both');
 
 	this.service = new Service.ContactSensor(this.name);
 
@@ -29,20 +31,22 @@ function ContactGPIOSensor(log, config) {
 
 ContactGPIOSensor.prototype.getState = function(callback) {
 	var val;
-	for (var i = 0; i < 10; i++) {
-		val = contactSensor.readSync();
+	for (var i = 0; i < this.retryCount; i++) {
+		val = this.contactSensor.readSync();
 		if (val == 1) {
 			break;
 		}
 	}
-	if (val == 1) {
-		val = 0;
-	} else if (val == 0) {
-		val = 1;
+	if (val === 1) {
+		// circuit is closed
+		val = Characteristic.ContactSensorState.CONTACT_DETECTED;
+	} else if (val === 0) {
+		// circuit is open
+		val = Characteristic.ContactSensorState.CONTACT_NOT_DETECTED;
 	}
 	callback(null, val);
-}
+};
 
 ContactGPIOSensor.prototype.getServices = function() {
   return [this.service];
-}
+};
