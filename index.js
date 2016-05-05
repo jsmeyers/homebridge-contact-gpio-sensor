40fe3b8544f8a38ec0a74d6209418ede3afd0156
Luca Becker <luca.becker@me.com>
2016-05-05 11:09:28 +0200

    pushing changes to homekit

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ var Gpio = require('onoff').Gpio;
 
 var RETRY_COUNT = 10;
 
+function translate(value) {
+	var val;
+	if (value === 1) {
+		// circuit is closed
+		val = Characteristic.ContactSensorState.CONTACT_DETECTED;
+	} else if (value === 0) {
+		// circuit is open
+		val = Characteristic.ContactSensorState.CONTACT_NOT_DETECTED;
+	}
+	return val;
+}
+
 
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
@@ -22,11 +34,22 @@ function ContactGPIOSensor(log, config) {
 	this.retryCount = config.retryCount || RETRY_COUNT;
 	this.contactSensor = new Gpio(this.pinId, 'in', 'both');
 
+
+
 	this.service = new Service.ContactSensor(this.name);
 
 	this.service.getCharacteristic(Characteristic.ContactSensorState)
 		.on('get', this.getState.bind(this));
 
+	var that = this;
+	this.contactSensor.watch(function(err, value) {
+		that.service.getCharacteristic(Characteristic.ContactSensorState)
+			.setValue(translate(value));
+	});
+
+	process.on('SIGINT', function () {
+		this.contactSensor.unexport();
+	});
 }
 
 ContactGPIOSensor.prototype.getState = function(callback) {
@@ -37,13 +60,7 @@ ContactGPIOSensor.prototype.getState = function(callback) {
 			break;
 		}
 	}
-	if (val === 1) {
-		// circuit is closed
-		val = Characteristic.ContactSensorState.CONTACT_DETECTED;
-	} else if (val === 0) {
-		// circuit is open
-		val = Characteristic.ContactSensorState.CONTACT_NOT_DETECTED;
-	}
+	val = translate(val);
 	callback(null, val);
 };
 
