232c2b9f836560e52c50938ad4650a0c5a556b20
Luca Becker <luca.becker@me.com>
2016-05-15 00:04:44 +0200

    updated package.json and counter for minutes the window/door was opened on that day

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ var Characteristic;
 var HomebridgeAPI;
 var Gpio = require('onoff').Gpio;
 var inherits = require('util').inherits;
+var schedule = require('node-schedule');
 
 var RETRY_COUNT = 10;
 
 function translate(value) {
-	var val;
-	if (value === 1) {
-		// circuit is closed
-		val = Characteristic.CurrentDoorState.CLOSED;
-	} else if (value === 0) {
-		// circuit is open
-		val = Characteristic.CurrentDoorState.OPEN;
-	}
-	return val;
+    var val;
+    if (value === 1) {
+        // circuit is closed
+        val = Characteristic.CurrentDoorState.CLOSED;
+    } else if (value === 0) {
+        // circuit is open
+        val = Characteristic.CurrentDoorState.OPEN;
+    }
+    return val;
 }
 
 
@@ -29,78 +30,127 @@ module.exports = function(homebridge) {
 };
 
 function ContactGPIOSensor(log, config) {
-	this.log = log;
-	this.name = config.name;
-	this.pinId = config.pinId;
-	this.retryCount = config.retryCount || RETRY_COUNT;
-	this.contactSensor = new Gpio(this.pinId, 'in', 'both');
-	this.openedCounter = 0;
+    var that = this;
+    this.log = log;
+    this.name = config.name;
+    this.pinId = config.pinId;
+    this.retryCount = config.retryCount || RETRY_COUNT;
+    this.contactSensor = new Gpio(this.pinId, 'in', 'both');
+    this.openedCounter = 0;
+
+    this.lastOpenedTimestamp = (this.contactSensor.readSync() == 1) ? null : new Date();
+    this.msCounter = 0;
 
 
-	EveTimesOpened = function () {
-	    //todo: only rough guess of extreme values -> use correct min/max if known
-	    Characteristic.call(this, 'Times Opened', 'E863F129-079E-48FF-8F27-9C2605A29F52');
-	    this.setProps({
-	        format: Characteristic.Formats.UINT8,
-	        minValue: 0,
-	        minStep: 1,
-	        perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-	    });
-	    this.value = this.getDefaultValue();
-	};
-	inherits(EveTimesOpened, Characteristic);
+    var j = schedule.scheduleJob({hour: 00, minute: 00}, function(){
+        that.msCounter = 0;
+    });
 
-	this.eve_characteristic_times_opened = EveTimesOpened;
+    EveTimesOpened = function () {
+        Characteristic.call(this, 'Times Opened', 'E863F129-079E-48FF-8F27-9C2605A29F52');
+        this.setProps({
+            format: Characteristic.Formats.UINT8,
+            minValue: 0,
+            minStep: 1,
+            perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+        });
+        this.value = this.getDefaultValue();
+    };
+    inherits(EveTimesOpened, Characteristic);
 
-	//Eve service (custom UUID)
-	EveDoorService = function (displayName, subtype) {
-	    Service.call(this, displayName, 'E863F003-079E-48FF-8F27-9C2605A29F52', subtype);
-	    // Required Characteristics
-	    this.addCharacteristic(Characteristic.CurrentDoorState);
-	    // Optional Characteristics
-	    this.addOptionalCharacteristic(EveTimesOpened);
-	};
-	inherits(EveDoorService, Service);
+    this.eve_characteristic_times_opened = EveTimesOpened;
 
 
-	this.eve_service = new EveDoorService(this.name);
+    MinutesOpened = function () {
+        Characteristic.call(this, 'Minutes Opened Today', '83D143BE-FC86-4D02-8F43-97E86080AE8C');
+        this.setProps({
+            format: Characteristic.Formats.UINT8,
+            unit : "min",
+            minValue: 0,
+            minStep: 1,
+            perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+        });
+        this.value = this.getDefaultValue();
+    };
+    inherits(MinutesOpened, Characteristic);
 
-	this.eve_service.getCharacteristic(Characteristic.CurrentDoorState)
-		.on('get', this.getState.bind(this));
+    this.custom_characteristic_minutes_opened = MinutesOpened;
 
-	this.eve_service.getCharacteristic(EveTimesOpened)
-		.on('get', this.getOpenedCounter.bind(this));
+    //Eve service (custom UUID)
+    EveDoorService = function (displayName, subtype) {
+        Service.call(this, displayName, 'E863F003-079E-48FF-8F27-9C2605A29F52', subtype);
+        // Required Characteristics
+        this.addCharacteristic(Characteristic.CurrentDoorState);
+        // Optional Characteristics
+        this.addOptionalCharacteristic(EveTimesOpened);
+        this.addOptionalCharacteristic(MinutesOpened);
+    };
+    inherits(EveDoorService, Service);
 
-	var that = this;
-	this.contactSensor.watch(function(err, value) {
-		that.openedCounter++;
-		that.eve_service.getCharacteristic(that.eve_characteristic_times_opened)
-			.setValue(Math.floor(that.openedCounter/2));
-		that.eve_service.getCharacteristic(Characteristic.CurrentDoorState)
-			.setValue(translate(value));
-	});
 
-	process.on('SIGINT', function () {
-		that.contactSensor.unexport();
-	});
+
+
+
+    this.eve_service = new EveDoorService(this.name);
+
+    this.eve_service.getCharacteristic(Characteristic.CurrentDoorState)
+        .on('get', this.getState.bind(this));
+
+    this.eve_service.getCharacteristic(EveTimesOpened)
+        .on('get', this.getOpenedCounter.bind(this));
+
+    this.eve_service.getCharacteristic(MinutesOpened)
+        .on('get', this.getMinutesOpenedCounter.bind(this));
+
+
+
+
+
+
+    this.contactSensor.watch(function(err, value) {
+        that.openedCounter++;
+        that.eve_service.getCharacteristic(that.eve_characteristic_times_opened)
+            .setValue(Math.floor(that.openedCounter/2));
+        that.eve_service.getCharacteristic(Characteristic.CurrentDoorState)
+            .setValue(translate(value));
+
+        var now = new Date();
+        if (value == 1) {
+            that.msCounter += (now - that.lastOpenedTimestamp);
+            that.lastOpenedTimestamp = null;
+        } else {
+            that.lastOpenedTimestamp = now;
+        }
+
+        // homebridge does not appear to appreciate the notification of custom characteristics
+        // that.eve_service.getCharacteristic(that.custom_characteristic_minutes_opened)
+        //     .setValue(Math.floor(this.msCounter/(1000 * 60)));
+    });
+
+    process.on('SIGINT', function () {
+        that.contactSensor.unexport();
+    });
 }
 
 ContactGPIOSensor.prototype.getState = function(callback) {
-	var val;
-	for (var i = 0; i < this.retryCount; i++) {
-		val = this.contactSensor.readSync();
-		if (val == 1) {
-			break;
-		}
-	}
-	val = translate(val);
-	callback(null, val);
+    var val;
+    for (var i = 0; i < this.retryCount; i++) {
+        val = this.contactSensor.readSync();
+        if (val == 1) {
+            break;
+        }
+    }
+    callback(null, translate(val));
 };
 
 ContactGPIOSensor.prototype.getOpenedCounter = function(callback) {
-	callback(null, Math.floor(this.openedCounter/2));
+    callback(null, Math.floor(this.openedCounter/2));
+};
+
+ContactGPIOSensor.prototype.getMinutesOpenedCounter = function(callback) {
+    callback(null, Math.floor(this.msCounter/(1000 * 60)));
 };
 
 ContactGPIOSensor.prototype.getServices = function() {
-  return [this.eve_service];
+    return [this.eve_service];
 };
